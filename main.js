f68c351e541dd2dfc60e2a5c10a17a29894fa919
joyin <joyin.li@qq.com>
2026-02-08 03:09:07 +0800

    add menu

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // filepath: c:\Users\joyin\Desktop\joyinSoftware\timeBoom\main.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -12,6 +12,18 @@ function createWindow() {
     }
   });
   win.loadFile('index.html');
+
+  // 添加右键菜单
+  win.webContents.on('context-menu', (event, params) => {
+    const isAlwaysOnTop = win.isAlwaysOnTop();
+    const menu = Menu.buildFromTemplate([
+      {
+        label: isAlwaysOnTop ? '取消置顶' : '置顶',
+        click: () => win.setAlwaysOnTop(!isAlwaysOnTop)
+      }
+    ]);
+    menu.popup();
+  });
 }
 
 app.whenReady().then(createWindow);
